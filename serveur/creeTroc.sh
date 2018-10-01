627a71f0297e2c0826c94a424e1539a3e4fc2a3d
tueuraurel <tueuraurel@hotmail.com>
2018-10-01 12:32:36 +0200

    ajout creeTroc.sh

---
+++ serveur/creeTroc.sh
@@ -0,0 +1,4 @@
+mongoimport --db TROC --collection Membres --file Membres.json --jsonArray --drop
+mongoimport --db TROC --collection Competences --file Competences.json --jsonArray --drop
+mongoimport --db TROC --collection Biens --file Biens.json --jsonArray --drop
+mongoimport --db TROC --collection Utilisations --file Utilisations.json --jsonArray --drop
