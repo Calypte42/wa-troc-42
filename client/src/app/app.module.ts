5ec1c8a6c212b46770c868f8aa9263eddac5d637
tueuraurel <tueuraurel@hotmail.com>
2018-10-15 11:43:41 +0200

    Correction app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,25 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { MembresModule } from './membres/membres.module';
 import { TrajetsModule } from './trajets/trajets.module';
+import { BiensModule} from './biens/biens.module';
+import { CompetencesModule} from './competences/competences.module';
+import { EmpruntModule} from './emprunt/emprunt.module';
 
 import { AppComponent } from './app.component';
-import { BiensComponent } from './biens/biens.component';
-import { CompetencesComponent } from './competences/competences.component';
-import { EmpruntComponent } from './emprunt/emprunt.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BiensComponent,
-    CompetencesComponent,
-    EmpruntComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MembresModule,
-    TrajetsModule
+    TrajetsModule,
+    BiensModule,
+    CompetencesModule,
+    EmpruntModule
   ],
   providers: [],
   bootstrap: [AppComponent]
