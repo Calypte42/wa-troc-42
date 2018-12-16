f4e3555902cd1c6a157560e16d10edd069a24ef1
tueuraurel <tueuraurel@hotmail.com>
2018-12-16 22:36:46 +0100

    Ajout de la cherche

--- client/src/app/biens/mes-biens/mes-biens.component.ts
+++ client/src/app/biens/mes-biens/mes-biens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef  } from '@angular/core';
 import { BiensService } from '../biens.service';
 import { EmpruntService } from '../../emprunt/emprunt.service'
 import { Router } from '@angular/router';
@@ -7,6 +7,10 @@ import { NgForm } from '@angular/forms';
 import { RequestOptions } from '@angular/http';
 
 import { MesCookies } from '../../mesCookies';
+
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
 
 
 @Component({
@@ -22,15 +26,50 @@ export class MesBiensComponent implements OnInit {
   private biens: Object[];
   private derniereUtilisation: Object[];
 
+  dataTable: any;
+
+
   constructor(
     private mesCookies: MesCookies,
     private biensService: BiensService,
     private empruntService : EmpruntService,
-    private router: Router) { }
+    private router: Router, private chRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.userMail = this.mesCookies.getUserMail();
-    this.biensService.getBiens("/avecUtilisations/membre/" + this.mesCookies.getUserMail()).subscribe(res => this.biens = res);
+    this.biensService.getBiens("/avecUtilisations/membre/" + this.mesCookies.getUserMail()).subscribe(res => {
+        this.biens = res;
+        this.chRef.detectChanges();
+        const table: any = $('#monTableau');
+         this.dataTable = table.DataTable({
+         "columnDefs": [
+             { "orderSequence": [ "asc" ], "targets": [ 0 ] },
+             { "orderSequence": [ "desc" ], "targets": [ 1 ] }
+         ],
+         language: {
+         processing:     "Traitement en cours...",
+         search:         "Rechercher&nbsp;:",
+         lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
+         info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+         infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+         infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+         infoPostFix:    "",
+         loadingRecords: "Chargement en cours...",
+         zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+         emptyTable:     "Aucune donnée disponible dans le tableau",
+         paginate: {
+             first:      "Premier",
+             previous:   "Pr&eacute;c&eacute;dent",
+             next:       "Suivant",
+             last:       "Dernier"
+         },
+         aria: {
+             sortAscending:  ": activer pour trier la colonne par ordre croissant",
+             sortDescending: ": activer pour trier la colonne par ordre décroissant"
+         }
+     }
+         });
+    });
   }
 
   versCreationBien() {
--- client/src/app/competences/mes-competences/mes-competences.component.ts
+++ client/src/app/competences/mes-competences/mes-competences.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Input,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit ,Input,Output,EventEmitter, ChangeDetectorRef} from '@angular/core';
 import {CompetencesService} from '../competences.service';
 import { Router } from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
@@ -7,6 +7,9 @@ import {RequestOptions} from '@angular/http';
 
 import {MesCookies} from '../../mesCookies';
 
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
 
 @Component({
   selector: 'app-mes-competences',
@@ -18,14 +21,48 @@ export class MesCompetencesComponent implements OnInit {
     private userMail : String;
 
     private competences: Object[];
-    
+
+    dataTable: any;
+
   constructor(private mesCookies:MesCookies,
        private competencesService: CompetencesService,
-        private router : Router) { }
+        private router : Router, private chRef: ChangeDetectorRef) { }
 
   ngOnInit() {
       this.userMail=this.mesCookies.getUserMail();
-      this.competencesService.getCompetences("/membre/"+this.mesCookies.getUserMail()).subscribe(res => this.competences = res);
+      this.competencesService.getCompetences("/membre/"+this.mesCookies.getUserMail()).subscribe(res => {
+          this.competences = res;
+          this.chRef.detectChanges();
+          const table: any = $('#monTableau');
+           this.dataTable = table.DataTable({
+           "columnDefs": [
+               { "orderSequence": [ "asc" ], "targets": [ 0 ] },
+               { "orderSequence": [ "desc" ], "targets": [ 1 ] }
+           ],
+           language: {
+           processing:     "Traitement en cours...",
+           search:         "Rechercher&nbsp;:",
+           lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
+           info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+           infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+           infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+           infoPostFix:    "",
+           loadingRecords: "Chargement en cours...",
+           zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+           emptyTable:     "Aucune donnée disponible dans le tableau",
+           paginate: {
+               first:      "Premier",
+               previous:   "Pr&eacute;c&eacute;dent",
+               next:       "Suivant",
+               last:       "Dernier"
+           },
+           aria: {
+               sortAscending:  ": activer pour trier la colonne par ordre croissant",
+               sortDescending: ": activer pour trier la colonne par ordre décroissant"
+           }
+       }
+           });
+      });
   }
 
   versCreationCompetence(){
